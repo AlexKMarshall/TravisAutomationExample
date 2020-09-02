edcb93ab958b724dd1cbd4e34a4c4ec500bbfae9
Alex Marshall <alex.k.marshall83@gmail.com>
2020-09-02 12:26:40 +0100

    Test adding a todo

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitForElement } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+test("renders title", () => {
+  render(<App />);
+  expect(
+    screen.getByRole("heading", /My Awesome Todo List/i)
+  ).toBeInTheDocument();
+});
+
+test("can add a task", async () => {
+  render(<App />);
+  const taskName = "My first task";
+  const inputBox = screen.getByRole("textbox");
+  await userEvent.type(inputBox, taskName);
+  expect(inputBox).toHaveValue(taskName);
+
+  await userEvent.click(screen.getByText(/save/i));
+
+  await waitForElement(() => screen.getByRole("listitem"));
+  expect(inputBox).toHaveValue("");
+  expect(screen.getByText(taskName)).toBeInTheDocument();
 });
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,6 +5,7 @@ import { faCheck, faQuestion } from "@fortawesome/free-solid-svg-icons";
 
 const Todos = () => {
   const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   const newTodo = (name) => ({
     id: Date.now(),
@@ -14,8 +15,10 @@ const Todos = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    const todo = newTodo(event.target["todo-input"].value);
+    console.log(inputValue);
+    const todo = newTodo(inputValue);
     setTodos((oldTodos) => [todo, ...oldTodos]);
+    setInputValue("");
   };
 
   return (
@@ -23,7 +26,13 @@ const Todos = () => {
       <h1>My Awesome Todo List</h1>
       <form onSubmit={onSubmit}>
         <label htmlFor="todo-input">Add a task:</label>
-        <input type="text" name="todo-input" id="todo-input" />
+        <input
+          type="text"
+          name="todo-input"
+          id="todo-input"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
         <button type="submit">Save</button>
       </form>
       <ul>
